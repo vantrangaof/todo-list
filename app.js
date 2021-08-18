14673a4014abccc49a8ad6b90b7c222c3aafc6d7
Trang Van <87851721+vantrangaof@users.noreply.github.com>
2021-08-18 23:59:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 document.addEventListener("DOMContentLoaded", getTodos);
